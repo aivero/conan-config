431bb77a0649a3e170956f9bc003a6ea301ea24c
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-06 14:33:03 +0100

    Use zip file instead of git to update

--- hooks/updater.py
+++ hooks/updater.py
@@ -2,4 +2,4 @@ import os
 
 
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
-    os.system("conan config install https://gitlab.com/aivero/public/conan/conan-config.git")
+    os.system("conan config install https://static-objects.gitlab.net/aivero/public/conan/conan-config/-/archive/1.0.0/conan-config-1.0.0.zip")
