13dc3131fc18e5635fb79d8d133a2a120c01e25c
Niclas Overby <noverby@prozum.dk>
2020-06-26 08:54:12 +0200

    Simplify url in updater hook

--- hooks/updater.py
+++ hooks/updater.py
@@ -2,4 +2,4 @@ import os
 
 
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
-    os.system("conan config install https://codeload.github.com/aivero/conan-config/zip/master -sf conan-config-master")
+    os.system("conan config install https://github.com/aivero/conan-config/archive/master.zip -sf conan-config-master")
