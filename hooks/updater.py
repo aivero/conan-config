fff3faa98cfcd7c3a70dcfce62aed23733eca7f2
Niclas Overby <noverby@prozum.dk>
2020-06-26 08:47:24 +0200

    Use Github Conan config repository for updater hook

--- hooks/updater.py
+++ hooks/updater.py
@@ -2,4 +2,4 @@ import os
 
 
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
-    os.system("conan config install https://static-objects.gitlab.net/aivero/public/conan/conan-config/-/archive/1.0.0/conan-config-1.0.0.zip")
+    os.system("conan config install https://codeload.github.com/aivero/conan-config/zip/master -sf conan-config-master")
