a286b5fbc51852759f349a72e71c826e186b1d5e
Niclas Overby <noverby@prozum.dk>
2020-07-24 10:20:50 +0200

    Create directories only if they do not exist

--- hooks/dev_package_creator.py
+++ hooks/dev_package_creator.py
@@ -48,7 +48,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
                 rel_path = os.path.relpath(root, lib_folder)
                 file_path = os.path.join(lib_folder, rel_path, file)
                 file_dest_path = os.path.join(files_folder, "lib", rel_path, file)
-                if os.path.exists(os.path.dirname(file_dest_path)):
+                if not os.path.exists(os.path.dirname(file_dest_path)):
                     os.makedirs(os.path.dirname(file_dest_path))
                 shutil.move(file_path, file_dest_path)
 
@@ -60,7 +60,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
                 rel_path = os.path.relpath(root, package_folder)
                 file_path = os.path.join(package_folder, rel_path, file)
                 file_dest_path = os.path.join(files_folder, rel_path, file)
-                if os.path.exists(os.path.dirname(file_dest_path)):
+                if not os.path.exists(os.path.dirname(file_dest_path)):
                     os.makedirs(os.path.dirname(file_dest_path))
                 shutil.move(file_path, file_dest_path)
 
