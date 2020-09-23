bc0b05e098715590cbc3f2c92d946c12ec5ca4db
Niclas Overby <noverby@prozum.dk>
2020-09-23 22:05:08 +0200

    Create dev/dbg packages depending on conan create package name

--- hooks/dbg_package_creator.py
+++ hooks/dbg_package_creator.py
@@ -1,27 +1,33 @@
 import os
 import re
 import shutil
-import pathlib
 
 TEMPLATE = """
 import os
+import shutil
 from conans import *
 
 class Conan(ConanFile):
     name = "{0}-dbg"
     version = "{1}"
-    license ={2}
+    license = {2}
     description = "{0} debug files"
-    settings ={3}
-    exports_sources = ["files/*"]
+    settings = {3}
+    build_requires = "{0}/{1}"
 
     def package(self):
-        self.copy("*", src="files")
+        pkg_rootpath = self.deps_cpp_info["{0}"].rootpath
+
+        # Move src, dbg
+        for folder in ("src", "dbg"):
+            path = os.path.join(pkg_rootpath, folder)
+            if os.path.exists(path):
+               shutil.copytree(path, os.path.join(self.package_folder, folder))
+
 
     def package_info(self):
         self.env_info.SOURCE_MAP.append("%s|%s" % (self.name, os.path.join(self.package_folder, "src")))
         self.env_info.DEBUG_PATH.append(os.path.join(self.package_folder, "dbg"))
-        self.cpp_info.srcsdirs = ["src"]
 """
 
 
@@ -29,9 +35,27 @@ def env_prepend(var, val, sep=os.pathsep):
     os.environ[var] = val + (sep + os.environ[var] if var in os.environ else "")
 
 
+def post_export(output, conanfile, conanfile_path, reference, **kwargs):
+    # Only create debug package when ending with -dbg
+    if not conanfile.name.endswith("-dbg"):
+        return
+
+    # Don't create dev package for bootstrap packages
+    if conanfile.name.startswith("bootstrap-"):
+        return
+
+    with open(conanfile_path, "w") as cfile:
+        content = TEMPLATE.format(
+            conanfile.name[:-4],
+            conanfile.version,
+            repr(conanfile.license),
+            repr(conanfile.settings),
+        )
+        cfile.write(content)
+
+
 def pre_build(output, conanfile, **kwargs):
     assert conanfile
-
     # Set debug prefix flags
     if not hasattr(conanfile, "source_folder"):  # Needs source directory
         return
@@ -59,24 +83,6 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
     if conanfile.name.endswith("-dev") or conanfile.name.endswith("-dbg"):
         return
 
-    # Don't create dev package for bootstrap packages
-    if conanfile.name.startswith("bootstrap-"):
-        return
-
-    build_folder = conanfile.build_folder
-    recipe_folder = os.path.join(
-        build_folder, f"{conanfile.name}-{conanfile.version}-dbg"
-    )
-    lockfile = os.path.join(recipe_folder, "lockfile")
-    files_folder = os.path.join(recipe_folder, "files")
-
-    # Create folder for dbg package files
-    if not os.path.exists(os.path.dirname(files_folder)):
-        os.makedirs(files_folder)
-
-    # Create lockfile
-    pathlib.Path(lockfile).touch()
-
     # Strip binaries and create debug files
     paths = (
         ("lib", r".*\.so.*"),
@@ -88,72 +94,53 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
         for root, _, files in os.walk(os.path.join(conanfile.package_folder, path[0])):
             for file in files:
                 if regex.match(file):
-                    debug_path = os.path.join(files_folder, "dbg", root[1:])
+                    debug_path = os.path.join(conanfile.package_folder, "dbg", root[1:])
                     if not os.path.exists(os.path.dirname(debug_path)):
                         os.makedirs(debug_path)
-                    bin_file = os.path.join(root, file)
                     debug_file = f"{os.path.join(debug_path, file)}.debug"
-                    os.system(f"objcopy --only-keep-debug {bin_file} {debug_file}")
-                    os.system(f"strip -g {bin_file}")
-                    os.system(f"objcopy --add-gnu-debuglink={debug_file} {bin_file}")
+                    # Extract debug info to debug file
+                    os.system(
+                        f"objcopy --only-keep-debug {os.path.join(root, file)} {debug_file}"
+                    )
+                    # Strip binary
+                    os.system(
+                        f"strip --strip-debug --strip-unneeded {os.path.join(root, file)}"
+                    )
+                    # Link binary to debug file
+                    os.system(
+                        f"objcopy --add-gnu-debuglink={debug_file} {os.path.join(root, file)}"
+                    )
 
     # Copy sources to package
     regex = re.compile(r".*\.(c|C|cc|cpp|cxx|c\+\+|h|H|hh|hpp|hxx|h\+\+|rs|y|l)$")
-    for root, _, files in os.walk(build_folder):
+    for root, _, files in os.walk(conanfile.build_folder):
         for file in files:
-            if (
-                regex.match(file)
-                and not f"{conanfile.name}-{conanfile.version}-dbg" in root
-            ):
-                rel_path = os.path.relpath(root, build_folder)
-                file_path = os.path.join(build_folder, rel_path, file)
-                file_dest_path = os.path.join(files_folder, "src", rel_path, file)
+            if regex.match(file):
+                rel_path = os.path.relpath(root, conanfile.build_folder)
+                file_path = os.path.join(conanfile.build_folder, rel_path, file)
+                file_dest_path = os.path.join(
+                    conanfile.package_folder, "src", rel_path, file
+                )
                 if not os.path.exists(os.path.dirname(file_dest_path)):
                     os.makedirs(os.path.dirname(file_dest_path))
-                shutil.move(file_path, file_dest_path)
+                shutil.copy(file_path, file_dest_path)
 
 
-def license_to_str(licenses):
-    res = ""
-    if isinstance(licenses, str):
-        licenses = [licenses]
-    for lic in licenses:
-        res += f' "{lic}",'
-    return res
+def pre_upload_package(output, conanfile_path, reference, package_id, remote, **kwargs):
+    package_folder = conanfile_path.replace(
+        "export/conanfile.py", f"package/{package_id}"
+    )
 
-
-def setting_to_str(setting):
-    res = ""
-    for (key, _) in setting.items():
-        if not "." in key:
-            res += f' "{key}",'
-    return res
-
-
-def post_package_info(output, conanfile, reference, **kwargs):
-    assert conanfile
-
-    # Don't create dev package for bootstrap packages
-    if conanfile.name.startswith("bootstrap-"):
+    # Don't cleanup package for bootstrap, development and debug packages
+    if (
+        reference.name.startswith("bootstrap-")
+        and reference.name.endswith("-dev")
+        and reference.name.endswith("-dbg")
+    ):
         return
 
-    build_folder = conanfile.package_folder.replace("/package/", "/build/")
-    recipe_folder = os.path.join(
-        build_folder, f"{conanfile.name}-{conanfile.version}-dbg"
-    )
-    files_folder = os.path.join(recipe_folder, "files")
-    lockfile = os.path.join(recipe_folder, "lockfile")
-    if os.path.exists(lockfile) and os.listdir(files_folder):
-        os.remove(lockfile)
-        with open(os.path.join(recipe_folder, "conanfile.py"), "w") as cfile:
-            content = TEMPLATE.format(
-                conanfile.name,
-                conanfile.version,
-                license_to_str(conanfile.license),
-                setting_to_str(conanfile.settings),
-            )
-            cfile.write(content)
-        ret = os.system(f"conan create {recipe_folder}")
-        print(f"Create return value ({conanfile.name}-dev): {ret}")
-        if ret != 0:
-            raise Exception(f"Could create package {conanfile.name}-dbg")
+    # Move src, dbg
+    for folder in ("src", "dbg"):
+        path = os.path.join(package_folder, folder)
+        if os.path.exists(path):
+            shutil.rmtree(path)
--- hooks/dev_package_creator.py
+++ hooks/dev_package_creator.py
@@ -1,55 +1,94 @@
 import os
 import re
 import shutil
-import pathlib
 
 TEMPLATE = """
+import os
+import re
+import shutil
 from conans import *
 
 class Conan(ConanFile):
     name = "{0}-dev"
     version = "{1}"
-    license ={2}
+    license = {2}
     description = "{0} development files"
-    settings ={3}
-    exports_sources = ["files/*"]
+    settings = {3}
+    build_requires = "{0}/{1}"
 
     def package(self):
-        self.copy("*", src="files")
+        pkg_rootpath = self.deps_cpp_info["{0}"].rootpath
+
+        # Move include
+        include_folder = os.path.join(pkg_rootpath, "include")
+        if os.path.exists(include_folder):
+           shutil.copytree(include_folder, os.path.join(self.package_folder, "include"))
+
+        # Move static libs
+        regex = re.compile(".*\.a")
+        lib_folder = os.path.join(pkg_rootpath, "lib")
+        for root, dirs, files in os.walk(lib_folder):
+            for file in files:
+                if regex.match(file):
+                    rel_path = os.path.relpath(root, lib_folder)
+                    file_path = os.path.join(lib_folder, rel_path, file)
+                    file_dest_path = os.path.join(self.package_folder, "lib", rel_path, file)
+                    if not os.path.exists(os.path.dirname(file_dest_path)):
+                        os.makedirs(os.path.dirname(file_dest_path))
+                    shutil.copy(file_path, file_dest_path)
+
+        # Move pkg-config files
+        regex = re.compile(".*\.pc")
+        for root, dirs, files in os.walk(pkg_rootpath):
+           for file in files:
+               if regex.match(file):
+                   rel_path = os.path.relpath(root, pkg_rootpath)
+                   file_path = os.path.join(pkg_rootpath, rel_path, file)
+                   file_dest_path = os.path.join(files_folder, rel_path, file)
+                   if not os.path.exists(os.path.dirname(file_dest_path)):
+                       os.makedirs(os.path.dirname(file_dest_path))
+                   shutil.copy(file_path, file_dest_path)
 """
 
 
-def post_package(output, conanfile, conanfile_path, **kwargs):
-    assert conanfile
-
-    # Don't create dev package for packages already ending with -dev or -dbg
-    if conanfile.name.endswith("-dev") or conanfile.name.endswith("-dbg"):
+def post_export(output, conanfile, conanfile_path, reference, **kwargs):
+    # Only create developement package when ending with -dev
+    if not conanfile.name.endswith("-dev"):
         return
 
     # Don't create dev package for bootstrap packages
     if conanfile.name.startswith("bootstrap-"):
         return
 
-    package_folder = conanfile.package_folder
-    recipe_folder = os.path.join(
-        conanfile.build_folder, f"{conanfile.name}-{conanfile.version}-dev"
+    with open(conanfile_path, "w") as cfile:
+        content = TEMPLATE.format(
+            conanfile.name[:-4],
+            conanfile.version,
+            repr(conanfile.license),
+            repr(conanfile.settings),
+        )
+        cfile.write(content)
+
+
+def pre_upload_package(output, conanfile_path, reference, package_id, remote, **kwargs):
+    package_folder = conanfile_path.replace(
+        "export/conanfile.py", f"package/{package_id}"
     )
-    dev_lockfile = os.path.join(recipe_folder, "lockfile")
-    files_folder = os.path.join(recipe_folder, "files")
 
-    # Create folder for dev package files
-    if not os.path.exists(os.path.dirname(files_folder)):
-        os.makedirs(files_folder)
+    # Don't cleanup package for bootstrap, development and debug packages
+    if (
+        reference.name.startswith("bootstrap-")
+        and reference.name.endswith("-dev")
+        and reference.name.endswith("-dbg")
+    ):
+        return
 
-    # Create lockfile
-    pathlib.Path(dev_lockfile).touch()
-
-    # Move include
+    # Delete include
     include_folder = os.path.join(package_folder, "include")
     if os.path.exists(include_folder):
-        shutil.move(include_folder, os.path.join(files_folder, "include"))
+        shutil.rmtree(include_folder)
 
-    # Move static libs
+    # delete static libs
     regex = re.compile(r".*\.a")
     lib_folder = os.path.join(package_folder, "lib")
     for root, _, files in os.walk(lib_folder):
@@ -57,10 +96,7 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
             if regex.match(file):
                 rel_path = os.path.relpath(root, lib_folder)
                 file_path = os.path.join(lib_folder, rel_path, file)
-                file_dest_path = os.path.join(files_folder, "lib", rel_path, file)
-                if not os.path.exists(os.path.dirname(file_dest_path)):
-                    os.makedirs(os.path.dirname(file_dest_path))
-                shutil.move(file_path, file_dest_path)
+                os.remove(file_path)
 
     # Move pkg-config files
     regex = re.compile(r".*\.pc")
@@ -69,53 +105,4 @@ def post_package(output, conanfile, conanfile_path, **kwargs):
             if regex.match(file):
                 rel_path = os.path.relpath(root, package_folder)
                 file_path = os.path.join(package_folder, rel_path, file)
-                file_dest_path = os.path.join(files_folder, rel_path, file)
-                if not os.path.exists(os.path.dirname(file_dest_path)):
-                    os.makedirs(os.path.dirname(file_dest_path))
-                shutil.move(file_path, file_dest_path)
-
-
-def license_to_str(licenses):
-    res = ""
-    if isinstance(licenses, str):
-        licenses = [licenses]
-    for lic in licenses:
-        res += f' "{lic}",'
-    return res
-
-
-def setting_to_str(setting):
-    res = ""
-    for (key, _) in setting.items():
-        if not "." in key:
-            res += f' "{key}",'
-    return res
-
-
-def post_package_info(output, conanfile, reference, **kwargs):
-    assert conanfile
-
-    # Don't create dev package for bootstrap packages
-    if conanfile.name.startswith("bootstrap-"):
-        return
-
-    build_folder = conanfile.package_folder.replace("/package/", "/build/")
-    recipe_folder = os.path.join(
-        build_folder, f"{conanfile.name}-{conanfile.version}-dev"
-    )
-    files_folder = os.path.join(recipe_folder, "files")
-    dev_lockfile = os.path.join(recipe_folder, "lockfile")
-    if os.path.exists(dev_lockfile) and os.listdir(files_folder):
-        os.remove(dev_lockfile)
-        with open(os.path.join(recipe_folder, "conanfile.py"), "w") as cfile:
-            content = TEMPLATE.format(
-                conanfile.name,
-                conanfile.version,
-                license_to_str(conanfile.license),
-                setting_to_str(conanfile.settings),
-            )
-            cfile.write(content)
-        ret = os.system(f"conan create {recipe_folder}")
-        print(f"Create return value ({conanfile.name}-dev): {ret}")
-        if ret != 0:
-            raise Exception(f"Could create package {conanfile.name}-dev")
+                os.remove(file_path)
