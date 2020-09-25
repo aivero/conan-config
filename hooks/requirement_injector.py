895ac404c499de81f710a6e22af4c8f5d77cae52
Niclas Overby <noverby@prozum.dk>
2020-09-25 13:04:57 +0200

    Add requirement injector hook

---
+++ hooks/requirement_injector.py
@@ -0,0 +1,32 @@
+import os
+import re
+import shutil
+
+
+def post_export(output, conanfile, conanfile_path, reference, **kwargs):
+    with open(conanfile_path, "r") as cfile:
+        content = cfile.read()
+
+    with open(conanfile_path, "w") as cfile:
+        # TODO: create cleaner injection with regex or ast manipulation
+
+        # Multi line requires
+        if "    requires = (\n" in content:
+            content = content.replace(
+                "    requires = (\n",
+                '    requires = (\n        ("generators/[^1.0.0]", "private"),\n',
+            )
+        # Single line requires
+        elif "    requires = (" in content:
+            content = content.replace(
+                "    requires = (",
+                '    requires = (("generators/[^1.0.0]", "private"), ',
+            )
+        # No requires
+        else:
+            content = content.replace(
+                "(ConanFile):\n",
+                '(ConanFile):\n    requires = (("generators/[^1.0.0]", "private"),)\n',
+            )
+
+        cfile.write(content)
