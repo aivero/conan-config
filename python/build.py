fa2669b2062c01a5efca062b30282add857a1507
Niclas Overby <noverby@prozum.dk>
2020-10-31 16:03:59 +0100

    build: add src_folder argument to get method fix

--- python/build.py
+++ python/build.py
@@ -60,7 +60,7 @@ class Recipe(ConanFile):
             cwd = self.src
         self.run(f"{command} {' '.join(args)}", cwd=cwd)
 
-    def get(self, url, dest_folder=None, src_folder):
+    def get(self, url, dest_folder=None, src_folder=None):
         if not dest_folder:
             dest_folder = self.src
         tools.get(url)
