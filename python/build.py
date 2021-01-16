ff17f31e2683df00f2efd86827ce03763b8a396b
Niclas Overby <noverby@prozum.dk>
2021-01-16 14:09:30 +0100

    build: replace relative paths in Cargo files

--- python/build.py
+++ python/build.py
@@ -309,6 +309,11 @@ class RustProject(Project):
         if version == "master":
             version = "0.0.0-master"
         cargo = toml.load("Cargo.toml")
+        if "dependencies" in cargo:
+            for name, dep in cargo["dependencies"].items():
+                if "path" in dep and ".." in dep["path"]:
+                    dep["path"] = dep["path"].replace("..", os.path.dirname(os.environ["ORIGIN_FOLDER"]))
+
         cargo["package"]["version"] = version
         with open('Cargo.toml', 'w') as f:
             toml.dump(cargo, f)
