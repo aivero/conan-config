efe216b94cbfc0dda7ae25e640ff20a18240f10a
Niclas Overby <noverby@prozum.dk>
2021-01-23 16:18:00 +0100

    hooks: add support for checking valid licenses

--- hooks/attribute_checker.py
+++ hooks/attribute_checker.py
@@ -1,6 +1,25 @@
+valid_licenses = (
+    "Apache",
+    "BSD",
+    "EPL",
+    "GPL",
+    "LGPL",
+    "MIT",
+    "MPL",
+    "PSF",
+    "Proprietary",
+    "ZLIB",
+    "custom",
+)
+
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     # Add warning if license and description is missing
     for field in ["license", "description"]:
         field_value = getattr(conanfile, field, None)
         if not field_value:
-            output.warn(f"Conanfile doesn't have '{field}'. It is recommended to add it as attribute")
+            output.error(f"Conanfile doesn't have '{field}'. Please add it.")
+    # Check if license is valid
+    licenses = [conanfile.license] if isinstance(conanfile.license, str) else conanfile.license
+    for license in licenses:
+        if license not in valid_licenses:
+            output.error(f"Conanfile license '{license}' is invalid. Please use one of {', '.join(valid_licenses)}.")
