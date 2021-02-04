8b70e03c9ee8320c2867d50e4238776dea268a92
Niclas Overby <noverby@prozum.dk>
2021-02-04 13:29:51 +0100

    build: use unrelated requirement mode for projects

--- python/build.py
+++ python/build.py
@@ -110,7 +110,8 @@ class Recipe(ConanFile):
     def conan_storage(self):
         if self._conan_storage:
             return self._conan_storage
-        self._conan_storage = call(sys.argv[0], ["config", "get", "storage.path"])[:-1]
+        self._conan_storage = call(
+            sys.argv[0], ["config", "get", "storage.path"])[:-1]
         return self._conan_storage
 
     def set_name(self):
@@ -218,7 +219,8 @@ class Recipe(ConanFile):
         )
         for (opt_name, opt_val) in opts.items():
             opt_data = next(
-                (opt_data for opt_data in opts_data if opt_name == opt_data["name"]),
+                (opt_data for opt_data in opts_data if opt_name ==
+                 opt_data["name"]),
                 None,
             )
             if not opt_data:
@@ -377,9 +379,10 @@ class RustRecipe(Recipe):
         cargo_toml = os.path.join(self.src, "Cargo.toml")
         if not os.path.exists(cargo_toml):
             return
-        manifest_raw = call("cargo", ["read-manifest", "--manifest-path", cargo_toml])
+        manifest_raw = call(
+            "cargo", ["read-manifest", "--manifest-path", cargo_toml])
         manifest = json.loads(manifest_raw)
-        # Automatically cdylibs and bins
+        # Automatically add cdylibs and bins
         for target in manifest["targets"]:
             if "cdylib" in target["kind"]:
                 target = f"lib{target['name']}.so"
@@ -414,6 +417,9 @@ class Project(Recipe):
     @property
     def src(self):
         return "."
+
+    def package_id(self):
+        self.info.requires.unrelated_mode()
 
 
 class GstProject(Project, GstRecipe):
