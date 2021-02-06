04c0c405b53de06a70b4f31f71a64d8cb45c4da9
Niclas Overby <noverby@prozum.dk>
2021-02-06 12:37:29 +0100

    alias creator: print when not creating alias

--- hooks/alias_creator.py
+++ hooks/alias_creator.py
@@ -1,5 +1,6 @@
 from build import *
 
+
 def write_metadata(path, key, val):
     metadata = {}
     metadata_path = os.path.join(path, METADATA_FILE)
@@ -7,15 +8,23 @@ def write_metadata(path, key, val):
         with open(metadata_path) as metadata_file:
             metadata = yaml.load(metadata_file)
     metadata[key] = val
-    with open(metadata_path, 'w') as metadata_file:
+    with open(metadata_path, "w") as metadata_file:
         yaml.dump(metadata, metadata_file)
+
 
 def pre_export(output, conanfile, conanfile_path, **kwargs):
     assert conanfile
     # Create branch alias for sha commit versions
     if re.match("^[0-9a-f]{40}$", conanfile.version):
-        output.info(f"Creating alias '{conanfile.name}/{branch()}' to '{conanfile.name}/{conanfile.version}'")
-        os.system(f"conan alias {conanfile.name}/{branch()} {conanfile.name}/{conanfile.version}")
+        output.info(
+            f"Creating alias '{conanfile.name}/{branch()}' to '{conanfile.name}/{conanfile.version}'"
+        )
+        os.system(
+            f"conan alias {conanfile.name}/{branch()} {conanfile.name}/{conanfile.version}"
+        )
+    else:
+        output.info(f"Not creating alias for '{conanfile.name}/{conanfile.version}'")
+
 
 def post_export(output, conanfile, conanfile_path, **kwargs):
     export_path = os.path.dirname(conanfile_path)
