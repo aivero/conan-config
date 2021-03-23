11b9c0366640b156ca904ad16f1088c481b4eff5
Niclas Overby <noverby@prozum.dk>
2021-03-23 10:44:17 +0100

    build: add support for relative paths

--- python/build.py
+++ python/build.py
@@ -103,14 +103,14 @@ class Recipe(ConanFile):
     def conan_home(self):
         if self._conan_home:
             return self._conan_home
-        self._conan_home = call(sys.argv[0], ["config", "home"])[:-1]
+        self._conan_home = call(os.path.abspath(sys.argv[0]), ["config", "home"])[:-1]
         return self._conan_home
 
     @property
     def conan_storage(self):
         if self._conan_storage:
             return self._conan_storage
-        self._conan_storage = call(sys.argv[0], ["config", "get", "storage.path"])[:-1]
+        self._conan_storage = call(os.path.abspath(sys.argv[0]), ["config", "get", "storage.path"])[:-1]
         return self._conan_storage
 
     def set_name(self):
