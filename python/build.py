2338fbd5705ad0bbf42f9d27f172b3842881abbe
Niclas Overby <niclas@overby.me>
2022-01-14 08:09:23 +0100

    build: get commit ref name and sha via gitlab

--- python/build.py
+++ python/build.py
@@ -66,8 +66,8 @@ def commit():
     global _commit
     if _commit:
         return _commit
-    if "GITHUB_SHA" in os.environ:
-        _commit = os.environ["GITHUB_SHA"]
+    if "CI_COMMIT_SHA" in os.environ:
+        _commit = os.environ["CI_COMMIT_SHA"]
         return _commit
     if os.path.exists(METADATA_FILE):
         with open(METADATA_FILE) as metadata_file:
@@ -84,8 +84,8 @@ def branch():
     global _branch
     if _branch:
         return _branch
-    if "GIT_REF" in os.environ:
-        _branch = os.environ["GIT_REF"]
+    if "CI_COMMIT_REF_NAME" in os.environ:
+        _branch = os.environ["CI_COMMIT_REF_NAME"]
         return _branch
     if os.path.exists(METADATA_FILE):
         with open(METADATA_FILE) as metadata_file:
