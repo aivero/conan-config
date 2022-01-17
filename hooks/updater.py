d2f5284f797536df78bf0cf56bcb1c40667c1cd5
Niclas Overby <niclas@overby.me>
2022-01-17 17:32:39 +0100

    updater: use gitlab url

--- hooks/updater.py
+++ hooks/updater.py
@@ -4,5 +4,5 @@ import sys
 
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     os.system(
-        f"{sys.argv[0]} config install https://github.com/aivero/conan-config/archive/gitlab.zip -sf conan-config-gitlab"
+        f"{sys.argv[0]} config install https://gitlab.com/aivero/open-source/conan-config/-/archive/master/conan-config-master.tar.gz -sf conan-config-master"
     )
